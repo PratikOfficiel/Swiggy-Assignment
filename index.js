ea4ee0d1d71158560dc2045be26f83bab9a4a93c
Pratik01-git <Pratik01-git>
2024-08-05 21:36:05 +0530

    deploying to github

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import MagicArena from "./modules/magicArena.module.js";
 
 // create 2 players with health, attack, strength and Name(optional) respectively. If no name is provided default Names will be given at the time of match.
 const p1 = new Player(50,10,5,"Goku");
-const p2 = new Player(100,5,10, "Naruto");
+const p2 = new Player(50,5,10, "Naruto");
 
 const game = new MagicArena(p2,p1);
 
